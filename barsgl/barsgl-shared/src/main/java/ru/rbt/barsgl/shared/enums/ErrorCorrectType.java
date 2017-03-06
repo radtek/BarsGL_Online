f4eda22d475ce382871a8b1a971997a0ed01e90a
Kichigin A <KichiginAA@rbtechnologies.ru>
2017-03-06 11:39:02 +0400

    Новые типы

--- barsgl/barsgl-shared/src/main/java/ru/rbt/barsgl/shared/enums/ErrorCorrectType.java
+++ barsgl/barsgl-shared/src/main/java/ru/rbt/barsgl/shared/enums/ErrorCorrectType.java
@@ -2,17 +2,16 @@ package ru.rbt.barsgl.shared.enums;
 
 import ru.rbt.barsgl.shared.HasLabel;
 
-import static ru.rbt.barsgl.shared.enums.ErrorCorrectType.CorrectType.NEW;
-import static ru.rbt.barsgl.shared.enums.ErrorCorrectType.CorrectType.REPROC;
+import static ru.rbt.barsgl.shared.enums.ErrorCorrectType.CorrectType.*;
 
 /**
  * Created by ER18837 on 28.02.17.
  */
 public enum ErrorCorrectType {
-    CLOSE_ONE(NEW), CLOSE_LIST(NEW), REPROCESS_ONE(REPROC), REPROCESS_LIST(REPROC);
+    CLOSE_ONE(NEW), CLOSE_LIST(NEW), REPROCESS_ONE(REPROC), REPROCESS_LIST(REPROC), EDIT_COMMENT(EDIT), EDIT_PST_COMMENT(EDIT);
 
     public enum CorrectType implements HasLabel{ 
-        NEW("Новое сообщение"), REPROC("Переобработка");
+        NEW("Новое сообщение"), REPROC("Переобработка"), EDIT("Редактирование");
 
         CorrectType(String label) {
             this.label = label;
