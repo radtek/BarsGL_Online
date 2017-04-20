a2c4521477d2847a07032905cc7a90a9ca6688be
Kichigin A <KichiginAA@rbtechnologies.ru>
2017-04-20 12:16:56 +0400

    - AccountBalanceUnloadTest - IGNORED

--- barsgl/barsgl-ejbparent/barsgl-ejbtest/src/test/java/ru/rbt/barsgl/ejbtest/AccountBalanceUnloadTest.java
+++ barsgl/barsgl-ejbparent/barsgl-ejbtest/src/test/java/ru/rbt/barsgl/ejbtest/AccountBalanceUnloadTest.java
@@ -1,12 +1,17 @@
 package ru.rbt.barsgl.ejbtest;
 
+import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
-import ru.rbt.barsgl.ejb.controller.operday.task.AccountBalanceUnloadTask;
+import ru.rbt.barsgl.ejb.common.mapping.od.Operday;
+import ru.rbt.barsgl.ejb.controller.operday.task.DwhUnloadParams;
 import ru.rbt.barsgl.ejb.controller.operday.task.balsep.AccountBalanceUnloadThree;
-import ru.rbt.barsgl.ejb.controller.operday.task.dem.UniAccountBalanceUnloadTask;
+import ru.rbt.barsgl.ejbcore.datarec.DataRecord;
 import ru.rbt.barsgl.ejbcore.mapping.job.SingleActionJob;
+import ru.rbt.barsgl.ejbcore.util.DateUtils;
 import ru.rbt.barsgl.ejbtest.utl.SingleActionJobBuilder;
 
+import java.util.Optional;
 import java.util.logging.Logger;
 
 /**
@@ -18,12 +23,28 @@ public class AccountBalanceUnloadTest extends AbstractTimerJobTest {
 
     public static final Logger logger = Logger.getLogger(AccountBalanceUnloadTest.class.getName());
 
-    @Test
+    @Test @Ignore("в настоящее время выгрузка в DWH не производится")
     public void testFull() throws Exception {
+
+        setOperday(DateUtils.dbDateParse("2017-02-09"), DateUtils.dbDateParse("2017-02-08")
+                , Operday.OperdayPhase.COB, Operday.LastWorkdayStatus.CLOSED);
+
+        checkCreateStep("P9", DateUtils.dbDateParse("2017-02-08"), "O");
+
+        baseEntityRepository.executeNativeUpdate("update gl_etldwhs set parvalue = '2'");
+
         SingleActionJob job = SingleActionJobBuilder.create()
                 .withClass(AccountBalanceUnloadThree.class)
                                   .withProps("operday=2016-03-12")
                                   .build();
         jobService.executeJob(job);
+
+        Optional<DataRecord> result = Optional.ofNullable(baseEntityRepository.selectFirst(
+                "select * from gl_etldwhs where PARDESC = ? and operday = ?"
+                , DwhUnloadParams.UnloadBalanceThree.getParamDesc(), getOperday().getCurrentDate()));
+
+        Assert.assertTrue(result.isPresent());
+        Assert.assertEquals("O", result.get().getString("parvalue"));
+
     }
 }
