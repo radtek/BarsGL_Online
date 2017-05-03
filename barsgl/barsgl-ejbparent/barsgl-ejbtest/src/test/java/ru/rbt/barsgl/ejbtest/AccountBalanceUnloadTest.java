573126a77264207d17cc4d1f971d054cba158f4e
Ivan Sevastyanov <SevastyanovII@rbtechnologies.ru>
2017-05-03 12:59:52 +0300

    -- AccountBalanceUnloadTest.java - IGNORED

--- barsgl/barsgl-ejbparent/barsgl-ejbtest/src/test/java/ru/rbt/barsgl/ejbtest/AccountBalanceUnloadTest.java
+++ barsgl/barsgl-ejbparent/barsgl-ejbtest/src/test/java/ru/rbt/barsgl/ejbtest/AccountBalanceUnloadTest.java
@@ -1,9 +1,8 @@
 package ru.rbt.barsgl.ejbtest;
 
+import org.junit.Ignore;
 import org.junit.Test;
-import ru.rbt.barsgl.ejb.controller.operday.task.AccountBalanceUnloadTask;
 import ru.rbt.barsgl.ejb.controller.operday.task.balsep.AccountBalanceUnloadThree;
-import ru.rbt.barsgl.ejb.controller.operday.task.dem.UniAccountBalanceUnloadTask;
 import ru.rbt.barsgl.ejbcore.mapping.job.SingleActionJob;
 import ru.rbt.barsgl.ejbtest.utl.SingleActionJobBuilder;
 
@@ -14,6 +13,7 @@ import java.util.logging.Logger;
  * Выгрузка данных о проводках и остатках в DWH
  * @fsd 8.2
  */
+@Ignore("Тестирование выгрузки в DWH: не используется")
 public class AccountBalanceUnloadTest extends AbstractTimerJobTest {
 
     public static final Logger logger = Logger.getLogger(AccountBalanceUnloadTest.class.getName());
