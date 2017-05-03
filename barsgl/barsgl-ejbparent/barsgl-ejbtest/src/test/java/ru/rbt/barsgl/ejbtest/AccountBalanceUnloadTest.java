44f209c7594976b0c7cf4a7faa6bb327a22dce2c
Ivan Sevastyanov <SevastyanovII@rbtechnologies.ru>
2017-05-03 13:20:08 +0300

    -- AccountBalanceUnloadTest.java - IGNORED - revert in master

--- barsgl/barsgl-ejbparent/barsgl-ejbtest/src/test/java/ru/rbt/barsgl/ejbtest/AccountBalanceUnloadTest.java
+++ barsgl/barsgl-ejbparent/barsgl-ejbtest/src/test/java/ru/rbt/barsgl/ejbtest/AccountBalanceUnloadTest.java
@@ -1,8 +1,9 @@
 package ru.rbt.barsgl.ejbtest;
 
-import org.junit.Ignore;
 import org.junit.Test;
+import ru.rbt.barsgl.ejb.controller.operday.task.AccountBalanceUnloadTask;
 import ru.rbt.barsgl.ejb.controller.operday.task.balsep.AccountBalanceUnloadThree;
+import ru.rbt.barsgl.ejb.controller.operday.task.dem.UniAccountBalanceUnloadTask;
 import ru.rbt.barsgl.ejbcore.mapping.job.SingleActionJob;
 import ru.rbt.barsgl.ejbtest.utl.SingleActionJobBuilder;
 
@@ -13,7 +14,6 @@ import java.util.logging.Logger;
  * Выгрузка данных о проводках и остатках в DWH
  * @fsd 8.2
  */
-@Ignore("Тестирование выгрузки в DWH: не используется")
 public class AccountBalanceUnloadTest extends AbstractTimerJobTest {
 
     public static final Logger logger = Logger.getLogger(AccountBalanceUnloadTest.class.getName());
@@ -22,8 +22,8 @@ public class AccountBalanceUnloadTest extends AbstractTimerJobTest {
     public void testFull() throws Exception {
         SingleActionJob job = SingleActionJobBuilder.create()
                 .withClass(AccountBalanceUnloadThree.class)
-                                  .withProps("operday=2016-03-12")
-                                  .build();
+                .withProps("operday=2016-03-12")
+                .build();
         jobService.executeJob(job);
     }
 }
