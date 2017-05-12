8080a4cb5434f93d666b279fd413b999be8689c3
KichiginAA <KichiginAA@rbtechnologies.ru>
2017-05-12 15:26:37 +0300

    MemorderTest

--- barsgl/barsgl-ejbparent/barsgl-ejbtest/src/test/java/ru/rbt/barsgl/ejbtest/MemorderTest.java
+++ barsgl/barsgl-ejbparent/barsgl-ejbtest/src/test/java/ru/rbt/barsgl/ejbtest/MemorderTest.java
@@ -133,7 +133,7 @@ public class MemorderTest extends AbstractTimerJobTest {
                             , accDt.substring(0,5) + "%", accCt.substring(0,5) + "%")
                     .getInteger(0);
             if (cnt == 0) {
-                baseEntityRepository.executeNativeUpdate("insert into F067_MASK (rowid, dat, datto, client_ind, dt_mask, ct_mask)" +
+                baseEntityRepository.executeNativeUpdate("insert into F067_MASK (row_id, dat, datto, client_ind, dt_mask, ct_mask)" +
                         " values (1, '2000-01-01', '2029-01-01', 'D', ?, ?)", accDt.substring(0,5) + "%", accCt.substring(0,5) + "%");
             }
         }
