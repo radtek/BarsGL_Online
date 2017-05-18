69c8326c0af9487adf5786836dd8f164e6ae5c73
samsonov <SamsonovAV@rbtechnologies.ru>
2017-05-18 11:41:24 +0300

    fix CacheController, PostingDateTest, PropertiesServiceTest

--- barsgl/barsgl-ejbparent/barsgl-ejb-core/src/main/java/ru/rbt/barsgl/ejbcore/CacheController.java
+++ barsgl/barsgl-ejbparent/barsgl-ejb-core/src/main/java/ru/rbt/barsgl/ejbcore/CacheController.java
@@ -1,11 +1,13 @@
 package ru.rbt.barsgl.ejbcore;
 
+import java.io.Serializable;
 import ru.rbt.ejbcore.repository.AbstractCachedRepository;
 
 import javax.enterprise.inject.Instance;
 import javax.inject.Inject;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import ru.rbt.ejbcore.mapping.BaseEntity;
 
 /**
  * Created by Ivan Sevastyanov on 14.07.2016.
@@ -15,7 +17,7 @@ public class CacheController {
     private static final Logger logger = Logger.getLogger(CacheController.class.getName());
 
     @Inject
-    private Instance<AbstractCachedRepository> cachedRepositories;
+    private Instance<AbstractCachedRepository<? extends BaseEntity, ? extends Serializable>> cachedRepositories;
 
     public void flushAllCaches() {
         for (AbstractCachedRepository repository : cachedRepositories) {
