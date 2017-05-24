bd2c5cc2d781dd3b05adfe8b2af23646c3897b8e
Ivan Sevastyanov <SevastyanovII@rbtechnologies.ru>
2017-05-24 14:55:55 +0300

    - ManualOperationAuthTest.java is resolved

--- barsgl/barsgl-ejbparent/barsgl-ejb/src/main/java/ru/rbt/barsgl/ejb/integr/oper/MovementCreateProcessorAlt.java
+++ barsgl/barsgl-ejbparent/barsgl-ejb/src/main/java/ru/rbt/barsgl/ejb/integr/oper/MovementCreateProcessorAlt.java
@@ -1,10 +1,15 @@
 package ru.rbt.barsgl.ejb.integr.oper;
 
+import ru.rbt.audit.controller.AuditController;
+import ru.rbt.audit.entity.AuditRecord;
 import ru.rbt.barsgl.ejb.integr.struct.MovementCreateData;
-import ru.rbt.ejbcore.DefaultApplicationException;
 
+import javax.ejb.EJB;
 import javax.enterprise.inject.Alternative;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.lang.String.format;
 
 /**
  * Created by er21006 on 23.05.2017.
@@ -12,8 +17,12 @@ import java.util.List;
 @Alternative
 public class MovementCreateProcessorAlt implements MovementCommunicator {
 
+    @EJB
+    private AuditController auditController;
+
     @Override
     public void sendRequests(List<MovementCreateData> mcdList) {
-        throw new DefaultApplicationException("Not implemented sending");
+        auditController.info(AuditRecord.LogCode.MovementCreate
+                , format("Message list was sent: %s", mcdList.stream().map(MovementCreateData::toString).collect(Collectors.joining(">:<"))));
     }
 }
