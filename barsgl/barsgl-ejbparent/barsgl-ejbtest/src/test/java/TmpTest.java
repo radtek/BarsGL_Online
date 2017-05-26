5ef28abfe4556e3c4f76e3df161270928d2e13d6
Ivan Sevastyanov <SevastyanovII@rbtechnologies.ru>
2017-05-26 13:57:41 +0300

    - промежуточный

--- barsgl/barsgl-ejbparent/barsgl-ejbtest/src/test/java/TmpTest.java
+++ barsgl/barsgl-ejbparent/barsgl-ejbtest/src/test/java/TmpTest.java
@@ -1,18 +1,12 @@
 import org.junit.Test;
-import ru.rbt.barsgl.ejb.entity.etl.EtlPackage;
-import ru.rbt.barsgl.ejb.entity.etl.EtlPosting;
-import ru.rbt.barsgl.ejbtest.AbstractTimerJobTest;
 
 /**
  * Created by ER22317 on 23.03.2017.
  */
-public class TmpTest extends AbstractTimerJobTest {
+public class TmpTest {
 
     @Test
     public void test01() throws Exception {
-        long stamp = System.currentTimeMillis();
-        EtlPackage pkg = (EtlPackage) baseEntityRepository.findById(EtlPackage.class, 147252);
-//        EtlPosting pst1
-
+        System.out.println("Echo remote testing");
     }
 }
