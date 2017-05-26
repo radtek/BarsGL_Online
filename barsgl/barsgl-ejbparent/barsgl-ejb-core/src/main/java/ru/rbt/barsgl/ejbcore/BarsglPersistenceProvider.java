95bf195473f2f188b68d9002687957bb625acc48
Andrew Samsonov <any@any.ru>
2017-05-26 14:55:08 +0300

    getting dataSource via annotation

--- barsgl/barsgl-ejbparent/barsgl-ejb-core/src/main/java/ru/rbt/barsgl/ejbcore/BarsglPersistenceProvider.java
+++ barsgl/barsgl-ejbparent/barsgl-ejb-core/src/main/java/ru/rbt/barsgl/ejbcore/BarsglPersistenceProvider.java
@@ -4,14 +4,11 @@
 package ru.rbt.barsgl.ejbcore;
 
 import ru.rbt.barsgl.shared.Repository;
-import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
-import javax.naming.InitialContext;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.sql.DataSource;
 import org.apache.log4j.Logger;
-import ru.rbt.ejbcore.DefaultApplicationException;
 import ru.rbt.ejbcore.PersistenceProvider;
 import ru.rbt.ejbcore.repository.AbstractBaseEntityRepository;
 
@@ -28,17 +25,11 @@ public class BarsglPersistenceProvider implements PersistenceProvider<Repository
     @PersistenceContext(unitName="RepAS400DataSource")
     protected EntityManager barsrepPersistence;
 
-//    @Resource(mappedName="jdbc/OracleGL")
+    @Resource(lookup="java:app/env/jdbc/OracleGL")
     private DataSource dataSource;
 
-//    @Resource(mappedName="jdbc/As400Rep")
+    @Resource(lookup="java:app/env/jdbc/As400Rep")
     private DataSource  barsrepDataSource;
-
-    @Resource(lookup = "java:app/env/BarsglDataSourceName")
-    private String barsglDataSourceName;
-
-    @Resource(lookup = "java:app/env/BarsrepDataSourceName")
-    private String barsrepDataSourceName;
     
     @Override
     public EntityManager getPersistence(Repository repository) throws Exception {
@@ -80,6 +71,7 @@ public class BarsglPersistenceProvider implements PersistenceProvider<Repository
         return persistence;
     }
 
+    /*
     @PostConstruct
     public void init() {
         dataSource = findConnection(barsglDataSourceName);
@@ -98,45 +90,6 @@ public class BarsglPersistenceProvider implements PersistenceProvider<Repository
             throw new DefaultApplicationException(e.getMessage(), e);
         }
     }
+    */
     
 }
-/*
-    @Resource(lookup = "java:app/env/jdbc/OracleGL")
-    @Resource(lookup = "java:app/env/jdbc/As400Rep")
-
-    need add to application.xml
-    WF
-    <resource-ref>
-        <res-ref-name>java:app/env/jdbc/OracleGL</res-ref-name>
-        <res-type>javax.sql.DataSource</res-type>
-        <res-auth>Container</res-auth>
-        <res-sharing-scope>Shareable</res-sharing-scope>
-        <lookup-name>java:/jdbc/OracleGL</lookup-name>
-    </resource-ref>
-
-    <resource-ref>
-        <res-ref-name>java:app/env/jdbc/As400Rep</res-ref-name>
-        <res-type>javax.sql.DataSource</res-type>
-        <res-auth>Container</res-auth>
-        <res-sharing-scope>Shareable</res-sharing-scope>
-        <lookup-name>java:/jdbc/As400Rep</lookup-name>
-    </resource-ref>
-
-    WLS    
-    <resource-ref>
-        <res-ref-name>java:app/env/jdbc/OracleGL</res-ref-name>
-        <res-type>javax.sql.DataSource</res-type>
-        <res-auth>Container</res-auth>
-        <res-sharing-scope>Shareable</res-sharing-scope>
-        <lookup-name>jdbc/OracleGL</lookup-name>
-    </resource-ref>
-
-    <resource-ref>
-        <res-ref-name>java:app/env/jdbc/As400Rep</res-ref-name>
-        <res-type>javax.sql.DataSource</res-type>
-        <res-auth>Container</res-auth>
-        <res-sharing-scope>Shareable</res-sharing-scope>
-        <lookup-name>jdbc/As400Rep</lookup-name>
-    </resource-ref>
-    
-*/    
--- barsgl/tasks/tasks-gwt-client/src/main/java/ru/rbt/tasks/gwt/client/tasks/TasksFormNew.java
+++ barsgl/tasks/tasks-gwt-client/src/main/java/ru/rbt/tasks/gwt/client/tasks/TasksFormNew.java
@@ -4,6 +4,7 @@ import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Image;
+import java.math.BigDecimal;
 import ru.rbt.security.gwt.client.AuthCheckAsyncCallback;
 import ru.rbt.tasks.gwt.client.TimerEntryPoint;
 import ru.rbt.grid.gwt.client.gridForm.GridForm;
@@ -106,7 +107,8 @@ public class TasksFormNew extends GridForm {
 
                 TimerJobWrapper timerJobs = new TimerJobWrapper();
 
-                timerJobs.setId((Long) row.getField(0).getValue());
+                //timerJobs.setId((Long) row.getField(0).getValue());
+                timerJobs.setId(((BigDecimal) row.getField(0).getValue()).longValueExact());
                 timerJobs.setProperties((String)row.getField(2).getValue());
 
                 timerJobs.setStartupType(JobStartupType.valueOf(row.getField(5).getValue().toString()));
