b0a0e20a47077cfbb5d0100a798a628143c1fa7d
Ivan Sevastyanov <SevastyanovII@rbtechnologies.ru>
2017-05-26 19:32:10 +0300

    - FIX Неверный счет '00057000010010103'. Ожидается в формате \d{8}.\d{11}
    (cherry picked from commit 31d2bf7)

--- barsgl/barsgl-ejbparent/barsgl-ejb/src/main/java/ru/rbt/barsgl/ejb/integr/acc/GLAccountService.java
+++ barsgl/barsgl-ejbparent/barsgl-ejb/src/main/java/ru/rbt/barsgl/ejb/integr/acc/GLAccountService.java
@@ -1,35 +1,36 @@
 package ru.rbt.barsgl.ejb.integr.acc;
 
 import org.apache.log4j.Logger;
+import ru.rbt.audit.controller.AuditController;
 import ru.rbt.barsgl.bankjar.Constants;
 import ru.rbt.barsgl.ejb.common.controller.od.OperdayController;
 import ru.rbt.barsgl.ejb.entity.acc.AccountKeys;
 import ru.rbt.barsgl.ejb.entity.acc.AccountKeysBuilder;
 import ru.rbt.barsgl.ejb.entity.acc.GLAccount;
 import ru.rbt.barsgl.ejb.entity.acc.GLAccountRequest;
-import ru.rbt.security.entity.access.PrmValue;
 import ru.rbt.barsgl.ejb.entity.dict.AccountingType;
 import ru.rbt.barsgl.ejb.entity.dict.BankCurrency;
 import ru.rbt.barsgl.ejb.entity.gl.GLOperation;
 import ru.rbt.barsgl.ejb.integr.bg.EtlPostingController;
 import ru.rbt.barsgl.ejb.repository.*;
-import ru.rbt.security.ejb.repository.access.PrmValueRepository;
-import ru.rbt.security.ejb.repository.access.SecurityActionRepository;
 import ru.rbt.barsgl.ejb.repository.dict.AccountingTypeRepository;
-import ru.rbt.audit.controller.AuditController;
 import ru.rbt.barsgl.ejb.security.UserContext;
+import ru.rbt.barsgl.shared.ErrorList;
+import ru.rbt.barsgl.shared.RpcRes_Base;
+import ru.rbt.barsgl.shared.Utils;
+import ru.rbt.barsgl.shared.account.ManualAccountWrapper;
+import ru.rbt.barsgl.shared.dict.FormAction;
 import ru.rbt.ejbcore.DefaultApplicationException;
 import ru.rbt.ejbcore.datarec.DataRecord;
 import ru.rbt.ejbcore.util.DateUtils;
 import ru.rbt.ejbcore.util.StringUtils;
 import ru.rbt.ejbcore.validation.ErrorCode;
 import ru.rbt.ejbcore.validation.ValidationError;
+import ru.rbt.security.ejb.repository.access.PrmValueRepository;
+import ru.rbt.security.ejb.repository.access.SecurityActionRepository;
+import ru.rbt.security.entity.access.PrmValue;
 import ru.rbt.shared.Assert;
-import ru.rbt.barsgl.shared.ErrorList;
 import ru.rbt.shared.ExceptionUtils;
-import ru.rbt.barsgl.shared.RpcRes_Base;
-import ru.rbt.barsgl.shared.account.ManualAccountWrapper;
-import ru.rbt.barsgl.shared.dict.FormAction;
 import ru.rbt.shared.enums.SecurityActionCode;
 
 import javax.ejb.EJB;
@@ -48,14 +49,13 @@ import java.util.function.Supplier;
 import static java.lang.String.format;
 import static org.apache.commons.lang3.StringUtils.defaultString;
 import static org.apache.commons.lang3.StringUtils.isEmpty;
+import static ru.rbt.audit.entity.AuditRecord.LogCode.Account;
 import static ru.rbt.barsgl.ejb.entity.gl.GLOperation.OperSide.C;
 import static ru.rbt.barsgl.ejb.entity.gl.GLOperation.OperSide.D;
-import static ru.rbt.audit.entity.AuditRecord.LogCode.Account;
 import static ru.rbt.ejbcore.util.StringUtils.substr;
 import static ru.rbt.ejbcore.validation.ErrorCode.*;
 import static ru.rbt.ejbcore.validation.ValidationError.initSource;
 import static ru.rbt.shared.enums.PrmValueEnum.Source;
-import ru.rbt.barsgl.shared.Utils;
 
 /**
  * Created by ER18837 on 03.08.15.
@@ -158,6 +158,8 @@ public class GLAccountService {
                 && keys.getGlSequence().toUpperCase().startsWith("TH")) {
             // заполнены и ключи и счет
             //glAccountController.fillAccountKeysMidas(operSide, dateOpen, keys);
+            BankCurrency currency = bankCurrencyRepository.refreshCurrency(keys.getCurrency());
+            keys.setCurrencyDigital(currency.getDigitalCode());
             String sAccType = keys.getAccountType();
             AccountingType accType = accountingTypeRepository.findById(AccountingType.class,sAccType);
             return Optional.ofNullable(glAccountController.findTechnicalAccountTH(accType,keys.getCurrency(),keys.getCompanyCode())).orElseGet(() -> {
@@ -165,7 +167,7 @@ public class GLAccountService {
                     checkNotStorno(operation, operSide);
                     return glAccountController.findOrCreateGLAccountTH(operation, accType,operSide, dateOpen, keys);
                 } catch (Exception e) {
-                    throw new DefaultApplicationException(e);
+                    throw new DefaultApplicationException(e.getMessage(), e);
                 }
             }).getBsaAcid();
         } else {
