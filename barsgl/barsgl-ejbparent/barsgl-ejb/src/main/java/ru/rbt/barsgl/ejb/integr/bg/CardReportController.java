2f64519ce5dd8e4dc0fe2f2f3e2d8a5d0991cbf4
KapitanovaLG <github.com>
2017-08-23 18:16:50 +0300

    вариант для тестирования, старый отчет должен выполняться

--- barsgl/barsgl-ejbparent/barsgl-ejb/src/main/java/ru/rbt/barsgl/ejb/integr/bg/CardReportController.java
+++ barsgl/barsgl-ejbparent/barsgl-ejb/src/main/java/ru/rbt/barsgl/ejb/integr/bg/CardReportController.java
@@ -1,6 +1,8 @@
 package ru.rbt.barsgl.ejb.integr.bg;
 
 import org.apache.commons.lang3.time.DateUtils;
+import ru.rbt.audit.controller.AuditController;
+import ru.rbt.audit.entity.AuditRecord;
 import ru.rbt.barsgl.ejb.entity.acc.AccCardId;
 import ru.rbt.barsgl.ejb.entity.acc.GLAccCard;
 import ru.rbt.barsgl.ejb.entity.acc.GLAccount;
@@ -10,6 +12,7 @@ import ru.rbt.barsgl.shared.RpcRes_Base;
 import ru.rbt.barsgl.shared.operation.CardReportWrapper;
 import ru.rbt.ejbcore.DefaultApplicationException;
 import ru.rbt.ejbcore.datarec.DataRecord;
+import ru.rbt.shared.ExceptionUtils;
 
 import javax.ejb.EJB;
 import javax.inject.Inject;
@@ -26,12 +29,16 @@ import java.util.List;
 public class CardReportController {
     private final static SimpleDateFormat databaseDate = new SimpleDateFormat("yyyy-MM-dd");
     private final static String finalDateStr = "2029-01-01";
+    private final Date finalDate = getFinalDate();
 
     @Inject
     private AccCardRepository cardRepository;
 
     @EJB
     GLAccountRepository accountRepository;
+
+    @EJB
+    AuditController auditController;
 
     // TODO заглушка
     public RpcRes_Base<CardReportWrapper> getCardReport(CardReportWrapper wrapper) {
@@ -42,54 +49,60 @@ public class CardReportController {
             return new RpcRes_Base<>(wrapper, true, String.format("Неверный формат даты '%s', требуется '%s'"
                     , wrapper.getPostDateStr(), wrapper.getDateFormat()));
         }
-        Date datStart = accountRepository.getDateStartCardPH();
-        if (null == datStart || dat.before(datStart)) {
-            wrapper.setReportSql(getReportSqlOld(dat, wrapper.getFilial()));
-            wrapper.setComment("Рассчет по остаткам");
-        } else {
-            createCardReport(dat, wrapper.getFilial());
-            wrapper.setReportSql(getReportSqlNew(dat, wrapper.getFilial()));
-            wrapper.setComment("Рассчет по оборотам");
+        try {
+            Date datStart = accountRepository.getDateStartCardPH();
+            if (null == datStart || dat.before(datStart)) {
+                wrapper.setReportSql(getReportSqlOld(dat, wrapper.getFilial()));
+                wrapper.setComment("Рассчет по остаткам");
+            } else {
+                accountRepository.executeInNewTransaction(persistence -> {
+                    createCardReport(dat, wrapper.getFilial()); return null;});
+                wrapper.setReportSql(getReportSqlNew(dat, wrapper.getFilial()));
+                wrapper.setComment("Рассчет по оборотам");
+            }
+            return new RpcRes_Base<>(wrapper, false, "");
+        } catch (Throwable t) {
+            auditController.error(AuditRecord.LogCode.CardReport, String.format("Ошибка при создании отчета по картотеке для филиала '%s' на дату '%s'"
+                    , wrapper.getFilial(), wrapper.getPostDateStr()), null, t);
+            return new RpcRes_Base<>(wrapper, true, ExceptionUtils.getErrorMessage(t));
         }
-        return new RpcRes_Base<>(wrapper, false, "");
     }
 
-    private void createCardReport(Date procdate, String filial) {
-        try {
-            Date finalDate = databaseDate.parse(finalDateStr);
-
+    private void createCardReport(Date procdate, String filial) throws SQLException {
             // TODO транзакции !!
             List<DataRecord> accList = cardRepository.getFilalTurnoversOnDate(filial, procdate);
-            String bsaAcidPrev = "";
-            Date datToPrev = finalDate;
+            String bsaAcidStep = "";
+            Date datToStep = finalDate;
             for(DataRecord res: accList) {
                 String bsaAcid = res.getString(0);
-                if (bsaAcid.equals(bsaAcidPrev) && datToPrev.before(finalDate))
+                if (bsaAcid.equals(bsaAcidStep) && datToStep.before(finalDate))
                     continue;           // повторно счет не обрабатываем, если в пред случае была не последняя запись по счету
-                bsaAcidPrev = bsaAcid;
+                bsaAcidStep = bsaAcid;
+                datToStep = finalDate;
 
                 Date pod = res.getDate(1);
                 BigDecimal turnover = res.getBigDecimal(2);
                 GLAccCard accCardWas = cardRepository.findById(GLAccCard.class, new AccCardId(bsaAcid, pod));
                 if (null != accCardWas) {
                     // запись с такой датой есть
-                    datToPrev = accCardWas.getEndDate();
+                    datToStep = accCardWas.getEndDate();
                     accCardWas.setTurnovers(turnover);
                     if (!accCardWas.getEndDate().before(finalDate)) {
                         // последняя запись по счету
                         cardRepository.update(accCardWas);
                     } else {
-                        // не последняя запись по счету
-                        // TODO 3.3
+                        // не последняя запись по счету - п. 3.3
+                        createAccTurnoversAfterDate(accCardWas);
                     }
                 } else {
                     // найти запись с максимальной датой < POD
                     Date maxBefore = cardRepository.getMaxDateBefore(bsaAcid, pod);
-                    if (null == maxBefore) {                        // это будет первая запись по счету
+                    if (null == maxBefore) {
+                        // это будет первая запись по счету
                         GLAccount glAcc = accountRepository.selectFirst(GLAccount.class, "from GLAccount a where a.bsaAcid = ?1", bsaAcid);
                         if (null == glAcc)
                             throw new DefaultApplicationException(String.format("Не найден счет '%s' в таблице GL_ACC", bsaAcid));
-                        GLAccCard accCardNew = cardRepository.createAccCardRecord(bsaAcid, pod, turnover, glAcc);
+                        GLAccCard accCardNew = cardRepository.createAccCardRecord(glAcc, pod, finalDate, turnover);
                         cardRepository.save(accCardNew);
                     } else {
                         GLAccCard accCardPrev = cardRepository.findById(GLAccCard.class,new AccCardId(bsaAcid, maxBefore));
@@ -98,28 +111,43 @@ public class CardReportController {
                             Date minAfter = cardRepository.getMinDateAfter(bsaAcid, pod);
                             datTo = DateUtils.addDays(minAfter, -1);
                         }
-                        Date datePrev = DateUtils.addDays(pod, -1);        // TODO проверить для первого дня месяца
+                        datToStep = datTo;
+                        Date datPrev = DateUtils.addDays(pod, -1);        // TODO проверить для первого дня месяца
 
-                        GLAccCard accCardNew = cardRepository.createAccCardRecord(bsaAcid, pod, datTo, turnover, accCardPrev);
-                        accCardPrev.setEndDate(datePrev);
-
+                        GLAccCard accCardNew = cardRepository.createAccCardRecord(accCardPrev, pod, datTo, turnover);
+                        accCardPrev.setEndDate(datPrev);
                         cardRepository.save(accCardPrev);
-                        cardRepository.save(accCardNew);
-                        if (datTo.before(finalDate)) {
-                            // это не последняя запись по счету
-                            // TODO 3.3
+
+                        if (!datTo.before(finalDate)) {
+                            // последняя запись по счету
+                            cardRepository.save(accCardNew);
+                        } else {
+                            // не последняя запись по счету - п. 3.3
+                            createAccTurnoversAfterDate(accCardNew);
                         }
                     }
                 }
-
-
-
             }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } catch (ParseException e) {
-            e.printStackTrace();
+    }
+
+    public void createAccTurnoversAfterDate(GLAccCard cardOne) throws SQLException {
+        String bsaacid = cardOne.getId().getBsaAcid();
+        Date dateOne = cardOne.getId().getStartDate();
+        List<DataRecord> accList = cardRepository.getAccTurnoversAfterDate(bsaacid, dateOne);
+        cardRepository.deleteAccBalanceAfterDate(bsaacid, dateOne);
+
+        GLAccCard accCardPrev = cardOne;
+        for (DataRecord res: accList) {
+            Date pod = res.getDate(1);
+            BigDecimal turnover = res.getBigDecimal(2);
+            GLAccCard accCardNew = cardRepository.createAccCardRecord(accCardPrev, pod, finalDate, turnover);
+            Date datPrev = DateUtils.addDays(pod, -1);        // TODO проверить для первого дня месяца
+            accCardPrev.setEndDate(datPrev);
+            cardRepository.save(accCardPrev);
+            accCardPrev = accCardNew;
         }
+        cardRepository.save(accCardPrev);
+
     }
 
     public String getReportSqlOld(Date dat, String filial) {
@@ -148,6 +176,13 @@ public class CardReportController {
                 "    where a.cbcc = '%s' and a.dat <= '%s' and a.datto >= '%s'" +
                 " group by a.ccy, a.branch, a.subdealid" +
                 " order by a.subdealid, a.branch, a.ccy", filial, dateStr, dateStr);
+    }
 
+    private Date getFinalDate() {
+        try {
+            return databaseDate.parse(finalDateStr);
+        } catch (ParseException e) {
+            return null;
+        }
     }
 }
