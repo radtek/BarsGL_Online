651cbc4b7eaed8f5006a5fe7649793ef31f8d72c
Ivan Sevastyanov <SevastyanovII@rbtechnologies.ru>
2017-09-27 10:25:34 +0300

    - суммы проводок по ABS

--- barsgl/barsgl-ejbparent/barsgl-ejb/src/main/resources/ru/rbt/barsgl/ejb/controller/operday/task/stamt/del/stamt_deleted_result.sql
+++ barsgl/barsgl-ejbparent/barsgl-ejb/src/main/resources/ru/rbt/barsgl/ejb/controller/operday/task/stamt/del/stamt_deleted_result.sql
@@ -1,6 +1,6 @@
 insert into GL_STMDEL (	PCID, POSTINGDATE , VALUEDATE , DID , CID , DCBACCOUNT
     , CCBACCOUNT, HOSTSYSTEM, DEALID , PSID , DAMOUNT , CAMOUNT , DCUR , CCUR)
-select pcid.pcid, dt.postingdate, dt.valuedate, dt.pid did, ct.pid cid, dt.bsaacid dcbaccount, ct.bsaacid ccbaccount, dt.hostsystem, dt.dealid, dt.pcid, dt.amount damount, ct.amount camount, dt.ccy dcur, ct.ccy ccur
+select pcid.pcid, dt.postingdate, dt.valuedate, dt.pid did, ct.pid cid, dt.bsaacid dcbaccount, ct.bsaacid ccbaccount, dt.hostsystem, dt.dealid, dt.pcid, abs(dt.amount) damount, abs(ct.amount) camount, dt.ccy dcur, ct.ccy ccur
   from (select distinct pcid from session.TMP_PCID_DEL) pcid
  join session.TMP_PCID_DEL dt on pcid.pcid = dt.pcid and dt.amount < 0
  join session.TMP_PCID_DEL ct on pcid.pcid = ct.pcid and ct.amount > 0
