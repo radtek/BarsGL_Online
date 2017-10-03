588b95e3e10fac919919adfae08e7c5d5ada640c
KichiginAA <KichiginAA@rbtechnologies.ru>
2017-10-03 12:32:21 +0300

    Убрал пункт меню План Счетов ОФР по Midas т.к более не используется.

--- rbt-base/rbt-shared/src/main/java/ru/rbt/shared/enums/UserMenuCode.java
+++ rbt-base/rbt-shared/src/main/java/ru/rbt/shared/enums/UserMenuCode.java
@@ -7,7 +7,7 @@ public enum UserMenuCode {
       SystemMenu, Accounting, Dictionary, Upload, Task, Operday, Access, User, Role
     , Backvalue, Audit, PLAccount, CBAccount, Operation, Posting, OperSemiposting
     , ErrorOper, UnloadAccountBalance, AEIncomeMsg, FileIncomeMsg, PostingSource
-    , TermCode, AccountingType, PlanAccountingType, PlanAccountOfr, PropertyType, Branch
+    , TermCode, AccountingType, PlanAccountingType,/* PlanAccountOfr,*/ PropertyType, Branch
     , UnloadStamtConfig, Acc2Deals, TemplateOper, OperInpConfirm, OperInpHistory, ProfitCentr
     , DomesticPlan, AccTypeParts, LoaderControl, BufferSync, AcodMidas, PLAccountAcctype, PLAccountMidas
     , Monitoring, CheckCardsRemains, FileIncomePkg, OperAuthorize, TechAccounting, TechAccount, TechPosting
