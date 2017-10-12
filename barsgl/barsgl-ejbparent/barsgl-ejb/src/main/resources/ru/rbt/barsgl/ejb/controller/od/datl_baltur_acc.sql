a76c40fa4ad478b01880477800371e6305d0b2d9
KapitanovaLG <github.com>
2017-10-12 14:34:55 +0300

    исправлены SQL в методе DatLCorrector.correctDatL

--- barsgl/barsgl-ejbparent/barsgl-ejb/src/main/resources/ru/rbt/barsgl/ejb/controller/od/datl_baltur_acc.sql
+++ barsgl/barsgl-ejbparent/barsgl-ejb/src/main/resources/ru/rbt/barsgl/ejb/controller/od/datl_baltur_acc.sql
@@ -1,5 +1,5 @@
-SELECT B.*, CHAR(DAT) || ' ' || B.BSAACID || ' ' || value(B.ACID, 'NULL') ID
-  FROM BALTUR B
- WHERE B.ACID = ? AND B.BSAACID = ?
-   AND (DATTO = ? OR DAT >= ?)
- ORDER BY DAT
+SELECT B.*, TO_CHAR(DAT) || ' ' || B.BSAACID || ' ' || COALESCE(B.ACID, 'NULL') ID
+                                         FROM BALTUR B
+                                        WHERE B.ACID = ? AND B.BSAACID = ?
+                                          AND (DATTO = ? OR DAT >= ?)
+                                        ORDER BY DAT
