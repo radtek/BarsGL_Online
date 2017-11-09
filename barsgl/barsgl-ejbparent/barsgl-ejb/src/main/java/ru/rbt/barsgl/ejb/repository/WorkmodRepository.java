aae780bedef31b2728384d26e108eadc1b9e6e6f
Ivan Sevastyanov <SevastyanovII@rbtechnologies.ru>
2017-11-09 17:21:13 +0300

    - workmod.trim(code)

--- barsgl/barsgl-ejbparent/barsgl-ejb/src/main/java/ru/rbt/barsgl/ejb/repository/WorkmodRepository.java
+++ barsgl/barsgl-ejbparent/barsgl-ejb/src/main/java/ru/rbt/barsgl/ejb/repository/WorkmodRepository.java
@@ -4,10 +4,6 @@ import ru.rbt.barsgl.shared.Repository;
 import ru.rbt.ejbcore.datarec.DataRecord;
 import ru.rbt.ejbcore.repository.AbstractBaseEntityRepository;
 
-import javax.sql.DataSource;
-import java.sql.SQLException;
-import java.util.Date;
-
 /**
  * Created by SotnikovAV on 11.09.2017.
  */
@@ -17,7 +13,7 @@ public class WorkmodRepository extends AbstractBaseEntityRepository {
         final DataRecord countWorkproc = selectFirst(getDataSource(enumRepository),
                 "select count(1) cnt\n" +
                         "  from workmod w \n" +
-                        " where code = ? \n" +
+                        " where trim(code) = ? \n" +
                         " and run = 'Y' " +
                         " with NC", stepCode);
         return countWorkproc.getInteger("cnt") > 0;
