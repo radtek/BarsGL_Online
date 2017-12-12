97c8e0c3f9ba4d961a9ec57a8236e33ec71e05d3
KapitanovaLG <github.com>
2017-12-12 16:09:47 +0300

    Журнал очереди CustimerDetailNotify

---
+++ barsgl/barsgl-ejbparent/barsgl-ejb/src/main/java/ru/rbt/barsgl/ejb/repository/CustDNJournalRepository.java
@@ -0,0 +1,10 @@
+package ru.rbt.barsgl.ejb.repository;
+
+import ru.rbt.barsgl.ejb.entity.cust.CustDNJournal;
+import ru.rbt.ejbcore.repository.AbstractBaseEntityRepository;
+
+/**
+ * Created by er18837 on 12.12.2017.
+ */
+public class CustDNJournalRepository  extends AbstractBaseEntityRepository<CustDNJournal, Long> {
+}
