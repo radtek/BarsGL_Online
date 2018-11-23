5549b13774fcc03072ab22031106ba7e9e73d13e
Ivan Sevastyanov <SevastyanovII@rbtechnologies.ru>
2018-11-23 16:21:04 +0300

    - test 1

---
+++ barsgl/barsgl-ejbparent/barsgl-ejbtest/src/test/java/ru/rbt/barsgl/ejbtest/DiscountIT.java
@@ -0,0 +1,13 @@
+package ru.rbt.barsgl.ejbtest;
+
+/**
+ * Created by Ivan Sevastyanov on 22.11.2018.
+ */
+public class DiscountIT extends AbstractRemoteIT {
+
+    public void test() {
+
+
+
+    }
+}
